87fb1e94b57b6e169819321c5515ec41da1a97e4
Chetabahana <chetabahana@gmail.com>
2019-06-19 15:13:42 +0700

    update

--- branches/scripts/update/git.sh
+++ branches/scripts/update/git.sh
@@ -115,7 +115,7 @@ git fetch --prune origin master && git reset --hard origin/master
 export PATH=$HOME/.local/bin:$PATH && pipenv run tx pull --all > /dev/null
 find saleor -type f -print0 | xargs -0 sed -i 's|"localhost:8000"|"www.chetabahana.com"|g'
 git status && git add . && git commit -m "Add support for ${NEXT}"
-#git push origin "${NEXT}" --force
+git push origin "${NEXT}" --force
 
 cd $HOME
 rm -rf saleor Tutorial-Buka-Toko
