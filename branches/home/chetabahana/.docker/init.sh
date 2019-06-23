e4b97316d007eaad99eacc089cf28db8c1cb5223
chetabahana <chetabahana@gmail.com>
2019-06-23 13:35:55 +0000

    sync branches

--- branches/home/chetabahana/.docker/init.sh
+++ branches/home/chetabahana/.docker/init.sh
@@ -5,18 +5,18 @@ if [ -d ~/.docker/compose ]; then
 fi
 
 cd ~/.docker
-sudo rm -rf compose Toko-Chetabahana
 eval `ssh-agent` && expect ~/.ssh/agent
 
 #avoid lack of update other than master
-git clone git@github.com:Chetabahana/compose.git
+rm -rf branches compose Toko-Chetabahana
+git clone git@github.com:Chetabahana/branches.git
 git clone git@github.com:MarketLeader/Toko-Chetabahana.git
-mv -f compose/home/chetabahana/.ssh Toko-Chetabahana/compose/home/chetabahana/
+
+mv Toko-Chetabahana/compose . && rm -rf Toko-Chetabahana
+mv -f branches/home/chetabahana/.ssh compose/home/chetabahana/
 
 cd ~/.docker
-rm -rf compose
-mv Toko-Chetabahana/compose .
-rm -rf Toko-Chetabahana
+rm -rf branches
 eval `ssh-agent -k`
 
 bash ~/.docker/compose/scripts/main.bash
