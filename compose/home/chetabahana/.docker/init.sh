c06dbceda432ad883c0cf5b6afcc590be61d1176
chetabahana <chetabahana@gmail.com>
2019-06-23 11:12:50 +0000

    sync compose

--- compose/home/chetabahana/.docker/init.sh
+++ compose/home/chetabahana/.docker/init.sh
@@ -4,20 +4,11 @@ if [ -d ~/.docker/compose ]; then
    docker-compose down --volumes
 fi
 
-cd ~/.docker
-sudo rm -rf compose Toko-Chetabahana
-eval `ssh-agent` && expect ~/.ssh/agent
-
-#avoid lack of update other than master
+cd ~/.docker && sudo rm -rf compose
+eval `ssh-agent` && expect ~/.ssh/agent && ssh-add -l
 git clone git@github.com:Chetabahana/compose.git
-git clone git@github.com:MarketLeader/Toko-Chetabahana.git
-mv -f compose/home/chetabahana/.ssh Toko-Chetabahana/compose/home/chetabahana/
-
-cd ~/.docker
-rm -rf compose
-mv Toko-Chetabahana/compose .
-rm -rf Toko-Chetabahana
 eval `ssh-agent -k`
 
 bash ~/.docker/compose/scripts/main.bash
+
 (sleep 3; sudo reboot) &
