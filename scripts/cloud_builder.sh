fa60bd6d523f2759288d570478ad0083f8d88991
Chetabahana <chetabahana@gmail.com>
2019-07-26 23:42:59 +0700

    ./scripts/cloud_builder.sh

--- scripts/cloud_builder.sh
+++ scripts/cloud_builder.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Adjust column numbers ('190') follow to your screen
-printf -v res %190s
-export hr=`printf '%s\n' "${res// /-}"`
-export hrd=`printf '%s\n' "${res// /=}"`
-
-# Unmark the 2 line below to see all commands available
-#echo -e "\n$hr\nPATH COMMANDS\n$hr"
-#compgen -c | xargs which -a | sort
-
-# Name of current step
-[ -f steps.csv ] && date +%s >> steps.csv || gcloud builds \
-describe ${1} --format 'value(steps.name)' > steps.csv
-
-# Take the last word only
-STEPNAME=$(head -n 1 steps.csv | cut -d ';' -f `wc -l < steps.csv`)
-BASENAME=$(basename $STEPNAME); BASENAME=${BASENAME##*-}; BASENAME=${BASENAME##*_};
-
-bash scripts/${5}/$BASENAME.env ${1} ${2} ${3} ${4} ${5}
+ls -al
+cat example.html
