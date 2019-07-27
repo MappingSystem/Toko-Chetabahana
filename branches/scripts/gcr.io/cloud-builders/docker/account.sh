ab30356ffaa93f7fa4472e3caa0bdf2f0fca3b61
chetabahana <chetabahana@gmail.com>
2019-07-27 13:32:28 +0000

    sync source

--- branches/scripts/gcr.io/cloud-builders/docker/account.sh
+++ branches/scripts/gcr.io/cloud-builders/docker/account.sh
@@ -10,14 +10,6 @@ id
 echo "$hr\nSSH FILES\n$hr"
 ls -lL /root/.ssh
 
-if [ $PROJECT_ID = 'chetabahana' ]; then  
-	echo "\n$hr\nAGENT\n$hr"
-	eval `ssh-agent` && apt-get update > /dev/null
-	apt-get --assume-yes install expect > /dev/null
-	[ $HOME != /root ] && ln -s $HOME/.ssh /root/.ssh 
-	expect /root/.ssh/agent > /dev/null && ssh-add -l
-fi
-
 if [ $PROJECT_ID = 'marketleader' ]  
 then 
 	export GIT=https://chetabahana:`cat $HOME/.ssh/github_token`@github.com
