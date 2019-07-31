fa65a4607a786cc2006d92316e7c73e4d81faadb
chetabahana <chetabahana@gmail.com>
2019-07-31 21:03:27 +0000

    sync source

--- branches/.codefresh/pipeline/builders/python/artifact.sh
+++ branches/.codefresh/pipeline/builders/python/artifact.sh
@@ -29,6 +29,7 @@ git pull --rebase origin master
 git reset --hard origin/master
 cd ..
 
+echo "\n$hr\nCOPYING\n$hr"
 find .io -type d -name $REPO -exec cp -frpvT {} $REPO \;
 
 FLOWNAME=Toko-Chetabahana
@@ -42,6 +43,7 @@ echo "\n$hr\nORIGIN REPOSITORY\n$hr"
 cd $REPO && ls -al .
 
 [ $HOME = /root ] && return
+echo "\n$hr\nPUSH REPOSITORY\n$hr"
 ln -s $HOME/.ssh/push /bin/push
 chmod +x /bin/push
 push $ORIGIN
