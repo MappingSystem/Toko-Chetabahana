df686b0edcde4ced208706e5bac56816cf31284b
chetabahana <chetabahana@gmail.com>
2019-08-08 01:55:58 +0000

    Add support for Google Cloud Builder

---
+++ saleor/.google/cloud/builders/python/account.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo "$hr\nWHOAMI\n$hr"
+whoami
+echo $HOME
+id
+
+echo "$hr\nSSH FILES\n$hr"
+ls -lL /root/.ssh
+
+echo "\n$hr\nHOME PROFILES\n$hr"
+ls -al $HOME
+
+echo "\n$hr\nALL REPOSITORY\n$hr"
+ls -al /
+
+echo "\n$hr\nCURRENT REPOSITORY\n$hr"
+pwd
+ls -al .
+
+echo "\n$hr\nCLONE ORIGIN\n$hr"
+REPO=$(basename $ORIGIN .git)
+rm -rf $REPO && git clone $ORIGIN $REPO
+
+cd $REPO
+git checkout master
+mv README.md README_original.md
+git branch -D Chetabahana && git branch -m Chetabahana
+sed -i '0,/!.gitignore/s//!.gitignore\n!.google/' .gitignore
+cd ..
+
+echo "\n$hr\nCOPYING\n$hr"
+FLOWNAME=Toko-Chetabahana
+WORKFLOW=$FLOWNAME/branches/.google
+FLOW_GIT=https://github.com/MarketLeader/$FLOWNAME.git
+
+rm -rf $FLOWNAME && git clone $FLOW_GIT
+cp -frpvT $WORKFLOW $REPO/.google
+find .io -type d -name $REPO -exec cp -frpvT {} $REPO \;
+
+echo "\n$hr\nORIGIN REPOSITORY\n$hr"
+cd $REPO && ls -al .
+
+[ $HOME = /root ] && return
+echo "\n$hr\nPUSH REPOSITORY\n$hr"
+bash $HOME/.ssh/push
