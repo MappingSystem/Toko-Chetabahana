7ee38c35746f7578a0e195b80544fbad6b6b29f2
chetabahana <chetabahana@gmail.com>
2019-08-10 08:47:56 +0000

    Add support for Google Cloud Builder

--- .google/cloud/builders/docker/artifact.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "\n$hr\nCURRENT OUTPUT\n$hr"
-OUTPUT_REPO=Toko-Chetabahana
-cd $BUILDER_OUTPUT/volume/$OUTPUT_REPO
-ls -al .
-
-echo "\n$hr\nPUSH OUTPUT\n$hr"
-bash $HOME/.ssh/push
--- .google/cloud/builders/gcloud/artifact.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUTPUT_REPO=Toko-Chetabahana
-git clone https://github.com/MarketLeader/$OUTPUT_REPO.git $BUILDER_OUTPUT/volume/$OUTPUT_REPO
-find .io -type d -name $OUTPUT_REPO -exec cp -fv {}/README.md . \;
-cp -frpT /workspace $BUILDER_OUTPUT/volume/$OUTPUT_REPO
-
-echo "\n$hr\nCURRENT OUTPUT\n$hr"
-ls -al $BUILDER_OUTPUT/volume/$OUTPUT_REPO
