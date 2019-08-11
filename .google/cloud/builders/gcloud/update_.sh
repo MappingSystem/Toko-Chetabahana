696962690a7cb04160e5db9fa75aab3ad0bf6c73
chetabahana <chetabahana@gmail.com>
2019-08-11 10:43:14 +0000

    Add support for Google Cloud Builder

--- .google/cloud/builders/gcloud/update_.sh
+++ .google/cloud/builders/gcloud/update_.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+[ -z "${INSTANCE_NAME}" ] && return
+echo "\n$hr\nINSTANCE PROFILES\n$hr"
+[ "$REPO_NAME" != "$INSTANCE_NAME" ] && return
+
 return
+INSTANCE_HOME="$PROJECT_ID@$INSTANCE_NAME:/home"
+for i in /workspace/.google/cloud/compute/home/*; do
+    rm -rvf $i/.ssh/*.enc $i/.ssh/known_hosts $i/.ssh/google_compute_*
+    gcloud compute scp --verbosity info --recurse --force-key-file-overwrite --zone $INSTANCE_ZONE $i $INSTANCE_HOME
+done
